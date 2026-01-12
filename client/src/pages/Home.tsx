db9cd924cb58828e5611a947d78df3a88c71538c
igortom-beta <tomas.cernicky@gmail.com>
2026-01-12 13:40:11 +0100

    Update comments in Home.tsx for clarity

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,14 +4,14 @@ export default function Home() {
   return (
     <div className="min-h-screen flex flex-col bg-white">
       <main className="flex-1">
-        {/* Your existing website content goes here */}
+        {/* Zde zůstane tvůj stávající obsah webu */}
         <div className="container mx-auto px-4 py-8">
           <h1 className="text-4xl font-bold mb-4">Lojzovy Paseky</h1>
           <p className="text-lg text-gray-600">Moderní bungalovy u Lipna</p>
         </div>
       </main>
       
-      {/* Floating Chat Widget */}
+      {/* Toto je ten důležitý řádek, který tam vloží asistenta */}
       <FloatingChatWidget />
     </div>
   );
